e442fd8a54292b8d83f850e4dcba4b379ff22169
Nexisato <978452096@qq.com>
2021-08-02 12:01:29 +0800

    updated:2021-08-02

---
+++ LeetCode/1337_The_Weakest_Rows_in_a_Matrix/1337.cpp
@@ -0,0 +1,61 @@
+/*
+LeetCode 1337: The Weakest Rows in a Matrix
+@Description:
+You are given an m x n binary matrix mat of 1's (representing soldiers) and 0's (representing civilians). 
+The soldiers are positioned in front of the civilians. 
+That is, all the 1's will appear to the left of all the 0's in each row.
+
+A row i is weaker than a row j if one of the following is true:
+- The number of soldiers in row i is less than the number of soldiers in row j.
+- Both rows have the same number of soldiers and i < j.
+
+Return the indices of the k weakest rows in the matrix ordered from weakest to strongest.
+
+Constraints:
+m == mat.length
+n == mat[i].length
+2 <= n, m <= 100
+1 <= k <= m
+matrix[i][j] is either 0 or 1.
+*/
+#include <bits/stdc++.h>
+using namespace std;
+/*
+思路：二分查找 + 堆
+*/
+class Solution {
+public:
+    vector<int> kWeakestRows(vector<vector<int>>& mat, int k) {
+        int m = mat.size(), n = mat[0].size();
+        vector<pair<int, int>> table;
+        for (int i = 0; i < m; ++i) {
+            int l = 0, r = n - 1, pos = -1;
+            while (l <= r) {
+                int mid = l + (r - l) / 2;
+                if (mat[i][mid] == 0)
+                    r = mid - 1;
+                else {
+                    pos = mid;
+                    l = mid + 1;
+                }
+            }
+            table.emplace_back(pos + 1, i); //最后一个1位置为pos，共pos+1个士兵
+        }
+        priority_queue<pair<int, int>, vector<pair<int, int>>, greater<pair<int, int>>> minHeap;
+        for (auto [count, index] : table) 
+            minHeap.push(pair<int, int>(count, index));
+        
+
+        vector<int> res;
+        for (int i = 0; i < k; ++i) {
+            res.emplace_back(minHeap.top().second);
+            minHeap.pop();
+        }
+        
+        return res;
+    }
+};
+int main() {
+
+    return 0;
+}
---
+++ LeetCode/746_Min_Cost_Climbing_Stairs/746.cpp
@@ -0,0 +1,36 @@
+/*
+LeetCode 746: Min Cost Climbing Stairs
+@Description:
+You are given an integer array cost where cost[i] is the cost of ith step on a staircase. 
+Once you pay the cost, you can either climb one or two steps.
+
+You can either start from the step with index 0, or the step with index 1.
+
+Return the minimum cost to reach the top of the floor.
+
+Constraints:
+2 <= cost.length <= 1000
+0 <= cost[i] <= 999
+*/
+#include <iostream>
+#include <vector>
+using namespace std;
+/*
+思路：动态规划
+间隔1-2位，取较小值累加
+*/
+class Solution {
+public:
+    int minCostClimbingStairs(vector<int>& cost) {
+        int n = cost.size();
+        vector<int> dp(n + 1, 0);
+        dp[0] = dp[1] = 0;
+        for (int i = 2; i <= n; ++i)
+            dp[i] = min(dp[i - 1] + cost[i - 1], dp[i - 2] + cost[i - 2]);
+        return dp[n];
+    }
+};
+int main() {
+
+    return 0;
+}
