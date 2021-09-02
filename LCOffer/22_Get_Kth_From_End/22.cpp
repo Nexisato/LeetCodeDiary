d1e2fe8889f51679a1b08db5eb95fc56a6ac8cfa
Nexisato <978452096@qq.com>
2021-09-02 12:37:43 +0800

    updated:2021-09-02 the last year

---
+++ LCOffer/22_Get_Kth_From_End/22.cpp
@@ -0,0 +1,41 @@
+/*
+剑指Offer 22: 链表中倒数第 k 个节点
+@Description:
+输入一个链表，输出该链表中倒数第k个节点。为了符合大多数人的习惯，本题从1开始计数，即链表的尾节点是倒数第1个节点。
+
+例如，一个链表有 6 个节点，从头节点开始，它们的值依次是 1、2、3、4、5、6。这个链表的倒数第 3 个节点是值为 4 的节点。
+*/
+#include <iostream>
+#include <vector>
+#include <algorithm>
+using namespace std;
+struct ListNode{
+    int val;
+    ListNode *next;
+    ListNode() : val(0), next(nullptr) {}
+    ListNode(int x) : val(x), next(nullptr) {}
+    ListNode(int x, ListNode *next) : val(x), next(next) {}
+};
+class Solution {
+public:
+    ListNode* getKthFromEnd(ListNode* head, int k) {
+        ListNode *fastPtr, *slowPtr;
+        int N = 1;
+        fastPtr = head;
+        while (fastPtr != nullptr) {
+            fastPtr = fastPtr->next;
+            ++N;
+        }
+        int count  = N - K;
+        slowPtr = head;
+        while (--count)
+            slowPtr = slowPtr->next;
+        return slowPtr; 
+    }
+};
+int main() {
+
+
+
+    return 0;
+}
---
+++ LeetCode/789_Escape_The_Ghosts/789.cpp
@@ -0,0 +1,57 @@
+/*
+LeetCode 789: Escape The Ghosts
+@Description:
+You are playing a simplified PAC-MAN game on an infinite 2-D grid. You start at the point [0, 0], 
+and you are given a destination point target = [xtarget, ytarget], which you are trying to get to. 
+There are several ghosts on the map with their starting positions given as an array ghosts, 
+where ghosts[i] = [xi, yi] represents the starting position of the ith ghost. All inputs are integral coordinates.
+
+Each turn, you and all the ghosts may independently choose to either move 1 unit in any of the four cardinal directions: 
+north, east, south, or west or stay still. All actions happen simultaneously.
+
+You escape if and only if you can reach the target before any ghost reaches you. 
+If you reach any square (including the target) at the same time as a ghost, it does not count as an escape.
+
+Return true if it is possible to escape, otherwise return false.
+
+Constraints:
+1 <= ghosts.length <= 100
+ghosts[i].length == 2
+-10^4 <= xi, yi <= 10^4
+There can be multiple ghosts in the same location.
+target.length == 2
+-10^4 <= xtarget, ytarget <= 10^4
+*/
+#include <iostream>
+#include <vector>
+using namespace std;
+/*
+思路：曼哈顿距离法
+1. 所有人都会按照各自最短的距离到达 target 
+2. 每个人到达 target 都是按照曼哈顿距离的走法到达
+3. 无需考虑各自按照怎样的路线走
+    - 当有阻碍者可以先于或同时与玩家到达目的地，则逃跑失败
+    - 当玩家先于所有阻碍者到达目的地时，则一定可以逃跑成功，可以利用反证法证明一定不会在路中间遇到  
+*/
+class Solution {
+public:
+    bool escapeGhosts(vector<vector<int>>& ghosts, vector<int>& target) {
+        int n = ghosts.size();
+        int targetX = target[0], targetY = target[1];
+        auto dist = [&](int x, int y) {
+            return abs(targetX - x) + abs(targetY - y);
+        };
+        int distPlayer = dist(0 , 0);
+        for (int i = 0; i < n; ++i) {
+            int ghostX = ghosts[i][0], ghostY = ghosts[i][1];
+            if (dist(ghostX, ghostY) <= distPlayer)
+                return false;
+        }
+        return true;
+    }
+};
+int main() {
+
+
+    return 0;
+}
