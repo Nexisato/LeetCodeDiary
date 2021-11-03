f4f54b386f04ef64aa65b05b90d690f3b6b5cfd0
nexisato <978452096@qq.com>
2021-11-04 00:17:36 +0800

    updated:2021-11-04 重新开张！

---
+++ LeetCode/367_Valid_Perfect_Square/367.cpp
@@ -0,0 +1,45 @@
+/*
+LeetCode 367: Valid Perfect Square
+@Description:
+Given a positive integer num, write a function which returns True if num is a perfect square else False.
+
+Follow up: Do not use any built-in library function such as sqrt.
+
+Constraints:
+1 <= num <= 2^31 - 1
+*/
+#include <iostream>
+#include <algorithm>
+#include <vector>
+using namespace std;
+/*
+思路：二分查找平方根
+*/
+class Solution {
+public:
+    bool isPerfectSquare(int num) {
+        int l = 1, r = num;
+        while (l <= r) {
+            int mid = l + (r - l) >> 1;
+            if ((long long) mid * mid > INT32_MAX)
+                r = mid - 1;
+            else {
+                if (mid * mid < num)
+                    l = mid + 1;
+                else if (mid * mid > num)
+                    r = mid - 1;
+                else
+                    return true;                
+            }
+        }
+        return false;
+    }
+};
+int main() {
+    Solution *ss = new Solution();
+    int num = 99;
+    bool ans = ss->isPerfectSquare(num);
+
+
+    return 0;
+}
