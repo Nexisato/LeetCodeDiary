07ab69a8c4f025a7f0e448ab897ce37d2aff573e
nexisato <978452096@qq.com>
2023-09-11 10:41:10 +0800

    add different os settings on vscode

---
+++ test.cpp
@@ -0,0 +1,23 @@
+#include <iostream>
+#include <vector>
+#include <algorithm>
+#include <unordered_map>
+using namespace std;
+
+
+int main() {
+    vector<int> nums(11, 0);
+    int n = 10;
+    int num = 11;
+    while (n) {
+        nums[n] = num;
+        ++num;
+        --n;
+    }
+    for(const auto& i : nums)
+        cout << i << " ";
+    cout << endl;
+    
+
+    return 0;
+}
