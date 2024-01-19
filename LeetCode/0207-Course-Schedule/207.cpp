0041f59c7775b36044b70e4cebe601a6af4b4155
nexisato <978452096@qq.com>
2024-01-19 11:58:38 +0800

    refactor: format all dirs & files

--- LeetCode/0207-Course-Schedule/207.cpp
+++ LeetCode/0207-Course-Schedule/207.cpp
@@ -1,18 +1,20 @@
 /*
- * @Date: 2024-01-19 10:59:56
+ * @Date: 2024-01-19 10:53:43
  * @Author: nexisato
- * @FilePath: /LeetCodeDiary/LeetCode/0210-Course-Schedule-II/210.cpp
+ * @FilePath: /LeetCodeDiary/LeetCode/0207-Course-Schedule/207.cpp
  * @Description: 
  */
 #include <iostream>
-#include <string>
 #include <vector>
+#include <string>
 #include <queue>
 using namespace std;
 
+
 class Solution {
 public:
-    vector<int> findOrder(int numCourses, vector<vector<int>>& prerequisites) {
+    // 拓扑排序
+    bool canFinish(int numCourses, vector<vector<int>>& prerequisites) {
         vector<vector<int>> edges(numCourses);
         vector<int> inDeg(numCourses, 0);
         // DAG construction
@@ -25,22 +27,20 @@ public:
             if (inDeg[i] == 0)
                 q.push(i);
         int cnt = 0;
-        vector<int> res;    // 用于存储拓扑排序结果 
         while (!q.empty()) {
             int cur = q.front();
             q.pop();
             ++cnt;
-            res.push_back(cur);
             for (const auto& next : edges[cur]) {
                 --inDeg[next];
                 if (inDeg[next] == 0)
                     q.push(next);
             }
         }
-        // 因为是全部的课程，所以如果不是全部都能上，就返回空
-        return cnt == numCourses ? res : vector<int>();
+        return cnt == numCourses;
     }
 };
 int main() {
+
     return 0;
 }
--- scripts/format.sh
+++ scripts/format.sh
@@ -1,13 +1,52 @@
-# @Date: 2024-01-19 11:00:34
+#!/bin/sh
+###
+ # @Date: 2024-01-19 11:00:34
  # @Author: nexisato
  # @FilePath: /LeetCodeDiary/scripts/format.sh
- # @Description
+ # @Description: 删除 *.exe 和 *.out 等二进制文件，并格式化目录
+ # @usage ./scripts/format.sh ${WORK_DIR} ${COMMAND}
+ #   ${WORK_DIR} 为工作目录
+ #   ${COMMAND} 为命令，可选值为 [delete_bin/format_name]
 ### 
 
-
-# delete all *.out/*.exe files
 WORK_DIR=$1
-find . -name "*.out" -type f -delete
-delete_files() {
-    find ${WORK_DIR} -type f \(-name "*.exe" -o -name "*.out" \) -exec rm -f {} \;
+
+# 检查是否提供了工作目录
+if [ -z "$WORK_DIR" ]; then
+    echo "Error: No directory provided."
+    exit 1
+fi
+
+function delete_bin() {
+    find "${WORK_DIR}" -name '*.exe' -type f -print -exec rm -f {} \;
+    find "${WORK_DIR}" -name '*.out' -type f -print -exec rm -f {} \;
 }
+
+# 格式化目录文件，将 '_ '改为 '-'
+function format_name() {
+    # 首先重命名文件, *.json 文件不重命名   
+    find "${WORK_DIR}" -type f -name '*_*' -not -name '*.json'| while read fname; do
+        newname=$(echo "${fname}" | tr '_' '-')
+        if [ "${fname}" != "${newname}" ] && [ ! -e "${newname}" ]; then
+            mv -v "${fname}" "${newname}"
+        fi
+    done
+
+    # 然后重命名目录
+    find "${WORK_DIR}" -type d -name '*_*' | while read dname; do
+        newdname=$(echo "${dname}" | tr '_' '-')
+        if [ "${dname}" != "${newdname}" ] && [ ! -e "${newdname}" ]; then
+            mv -v "${dname}" "${newdname}"
+        fi
+    done
+}
+
+# 执行命令
+if [ "$2" = "delete_bin" ]; then
+    delete_bin
+elif [ "$2" = "format_name" ]; then
+    format_name
+else
+    echo "Error: Unknown command or no command provided."
+    exit 1
+fi
