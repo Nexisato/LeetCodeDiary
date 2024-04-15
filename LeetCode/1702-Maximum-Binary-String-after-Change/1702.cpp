328e9c64cf91f8328f96fc4889dc9a8e11269be9
nexisato <978452096@qq.com>
2024-04-15 22:51:53 +0800

    update: 2024-04-15

---
+++ LeetCode/1702-Maximum-Binary-String-after-Change/1702.cpp
@@ -0,0 +1,32 @@
+#include <iostream>
+#include <string>
+using namespace std;
+
+// 00 在前缀补1，10 后移 1
+// 字符串最多只有一个 0
+class Solution {
+public:
+    string maximumBinaryString(string binary) {
+        const int n = binary.length();
+        int j = 0;
+
+        for (int i = 0; i < n; ++i) {
+            if (binary[i] == '0') {
+                while (j <= i || (j < n && binary[j] == '1')) ++j;
+                if (j < n) {
+                    binary[j] = '1';
+                    binary[i] = '1';
+                    binary[i + 1] = '0';
+                }
+
+            }
+        }
+
+        return binary;
+    }
+};
+int main() {
+
+
+    return 0;
+}
---
+++ LeetCode/2864-Maxium-Odd-Binary-Number/2864.cpp
@@ -0,0 +1,32 @@
+/*
+ * @Date: 2024-03-13 08:01:18
+ * @Author: nexisato
+ * @FilePath: /LeetCodeDiary/LeetCode/2864-Maxium-Odd-Binary-Number/2864.cpp
+ * @Description: 
+ */
+#include <iostream>
+#include <vector>
+#include <string>
+using namespace std;
+
+
+
+class Solution {
+public:
+    string maximumOddBinaryNumber(string s) {
+        int n = s.length();
+        int idx = 0;
+        for (int i = 0; i < n; ++i)
+            if (s[i] == '1')
+                swap(s[idx++], s[i]);
+        swap(s[idx - 1], s[n - 1]);
+        return s;
+    }
+};
+
+
+int main() {
+
+
+    return 0;
+}
