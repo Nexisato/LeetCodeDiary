77c1f1d28f78fac79bb2e6cc3fed6013e9caced6
nexisato <978452096@qq.com>
2024-07-17 17:22:30 +0800

    feat: update c++20 config

--- test.cpp
+++ test.cpp
@@ -2,10 +2,10 @@
 #include <iostream>
 #include <unordered_map>
 #include <vector>
-using namespace std;
+#include <ranges>
 
 int main() {
-	vector<int> nums(11, 0);
+	std::vector<int> nums(11, 0);
 	int n = 10;
 	int num = 11;
 	while (n) {
@@ -13,9 +13,11 @@ int main() {
 		++num;
 		--n;
 	}
-	for (const auto& i : nums)
-		cout << i << " ";
-	cout << endl;
+
+	std::ranges::sort(nums);
+	for (int i = 0; i < 11; ++i)
+		std::cout << nums[i] << "\t";
+	std::cout << std::endl;
 
 	return 0;
 }
