1c6be397a242f27261601bc89b44e75b07f4818f
Nexisato <978452096@qq.com>
2024-09-13 14:47:27 +0800

    fix: find max_num less n

--- TestRepo/24-09-06-Hyperparameter/q1.py
+++ TestRepo/24-09-06-Hyperparameter/q1.py
@@ -1,42 +1,70 @@
 import heapq
+from typing import List, Any, Dict, Set
 
-def find_max_number_less_than_n(n, a):
-    a.sort(reverse=True)
-    num_str = str(n)
-    max_heap = []
 
-    def dfs(digit_index, current_value):
-        if current_value > n:
-            return
-        if digit_index == len(num_str):
-            heapq.heappush(max_heap, (-current_value, current_value))  # 使用负数来实现最大堆
-            return
+# 保证 digits 有序，获取小于 target 的最大下标
+def get_max_digit_lower(digits: List[int], target: int) -> int:
+    l , r = 0, len(digits)
+    res = -1
+    while l < r:
+        mid = int(l + (r - l) / 2)
+        if digits[mid] < target:
+            res = digits[mid]
+            l = mid + 1
+        else:
+            r = mid
+    return res
 
-        current_digit = int(num_str[digit_index])
-        found_smaller = False
+# 基本思想：n 的每个数位，从高位开始，尝试先使用相同的数字
+# 到最后一位 或者 没有相同的数字时，尝试寻找比当前位更小的数字
+#   - 若找到，则该位置使用这个数字，后面的全部用 a 中最大值
+#   - 没找到，继续往高位回溯，每一位都尝试寻找小于 n 对应数位的最大值，直到第一位没找到，则目标数位总数比 n 小，全部用 a 中最大值
+# 
+def find_max_number_less_than_n(n: int, a: List[int]):
+    a.sort()
+    n_digits: List[int] = list(map(int, str(n)))
+    a_digits: Set[int] = set(a)
+    
+    t_digits: List[int] = [0] * len(n_digits)
 
-        for num in a:
-            if num < current_digit:
-                dfs(digit_index + 1, current_value * 10 + num)
-                found_smaller = True
-
-            elif num == current_digit:
-                dfs(digit_index + 1, current_value * 10 + num)
-
-        if not found_smaller:
-            for num in a[::-1]:
-                dfs(digit_index + 1, current_value * 10 + num)
-
-    dfs(0, 0)
-
-    if max_heap:
-        return max_heap[0][1]
-    else:
-        return -1
+    
+    for idx, num in enumerate(n_digits):
+        if idx < len(n_digits) - 1 and num in a_digits:
+            t_digits[idx] = num
+            continue
+        # 小于 num 的最大值
+        d = get_max_digit_lower(a, num)
+        if d > 0:
+            t_digits[idx] = d
+            break
+        
+        cur_idx = idx
+        while cur_idx >= 0:
+            t_digits[cur_idx] = 0
+            
+            d = get_max_digit_lower(a, n_digits[cur_idx])
+            if d > 0:
+                t_digits[cur_idx] = d
+                break
+            cur_idx -= 1
+            if cur_idx < 0:
+                t_digits = t_digits[:-1]
+        break
+        
+    target = 0
+    for t in t_digits:
+        target *= 10
+        if t > 0:
+            target += t
+            continue
+        target += a[-1]
+    
+    return target
 
 
 if __name__ == '__main__':
-    n = 23121
-    a = [2, 4, 9]
+    n = 5555
+    a = [5, 9]
+    target = 3
+    
     print(find_max_number_less_than_n(n, a))
-
